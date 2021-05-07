1c933779e623882999c7173839daaea38b0a4384
Chris Paterson <chrispaterson22@gmail.com>
2021-05-07 14:23:54 +0100

    WIP add new goal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css'
-import GoalsList from './componenets/GoalList'
+import GoalsList from './components/GoalList/GoalList'
+import NewGoal from './components/NewGoal/NewGoal'
 
 const App = () => {
+	const courseGoals = [
+		{
+			id: 'cg1',
+			text: 'Finish Course',
+		},
+		{
+			id: 'cg2',
+			text: 'Learn all about the Course Main Topic',
+		},
+		{
+			id: 'cg3',
+			text: 'Help other students in the course Q&A',
+		},
+	]
+
 	return (
 		<div className='course-goals'>
 			<h2>Course Coals</h2>
-			<GoalsList />
+			<NewGoal />
+			<GoalsList goals={courseGoals} />
 		</div>
 	)
 }
---
+++ src/components/GoalList/GoalList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './GoalList.css'
+
+const GoalList = props => {
+	return (
+		<ul className='goal-list'>
+			{props.goals.map(item => {
+				return <li key={item.id}>{item.text}</li>
+			})}
+		</ul>
+	)
+}
+
+export default GoalList
